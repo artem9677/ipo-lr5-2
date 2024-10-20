dd3edf2e67a150175ff63b53416b454f3559b74e
Artem <69763892+artem9677@users.noreply.github.com>
2024-10-20 17:19:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-string_1 = str(input("Введите первую строку: "))
+string_1 = str(input("Введите первую строку: ")) # ввод строк
 string_2 = str(input("Введите вторую строку: "))
 
-if string_1 == string_2:
+if string_1 == string_2: # проверка 1
     print("Строки не являются анаграммами.")
     quit()
 
-elif len(string_1)!=len(string_2):
+elif len(string_1)!=len(string_2): # проверка 2
     print("Строки не являются анаграммами.")
     quit()
 
-is_anagr = 0
+is_anagr = 0 # переменная, нужная для проверки, являются ли строки анаграммами
 
-for i in string_1:
+for i in string_1: # проверка одинаковых символов
     for j in string_2:
         if i == j:
             is_anagr+=1
             break
 
-if is_anagr == len(string_1):
+if is_anagr == len(string_1): # 
     print("Строки являются анаграммами.")
 else:
     print("Строки не явлются анаграммами.")
